37c3c6b03fd69ceb52293f53b6f8e1f52b91c5ed
miracle-halation <miraclehalation.9710@gmail.com>
2020-07-17 15:29:00 +0900

    メッセージ送信機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root 'rooms#index'
   resources :users, only: [:edit, :update]
-  resources :rooms, only: [:index, :new, :create]
+  resources :rooms, only: [:new, :create] do
+    resources :messages, only: [:index, :create]
+  end
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
